e432675c79af11f5044155170cb9be48e543e141
Daron Short <daron.short@hotmail.com>
2019-06-24 18:25:36 -0500

    Basic scaffold

---
+++ src/components/Board.js
@@ -0,0 +1,14 @@
+import React, { Componet } from "react";
+import Cell from "./Cell";
+
+class Board extends Componet {
+  render() {
+    return (
+      <div>
+        <Cell />
+      </div>
+    );
+  }
+}
+
+export default Board;
